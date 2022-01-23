34d532e2eeba69f97f3fd815b778ac3dbb7e5fee
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-23 18:55:52 -0500

    :tada: Finalized GenStage pipeline

--- lib/event_consumer.ex
+++ lib/event_consumer.ex
@@ -5,10 +5,41 @@ defmodule EventConsumer do
   require Logger
 
   def start_link(request) do
-    Logger.info("EventConsumer received #{request.request_id}")
-
     Task.start_link(fn ->
-      Logger.info("EventConsumer doing work!!")
+      decision = credit_decision(request)
+
+      Logger.info(
+        "Credit Decision for : #{inspect(decision.request_id)} is : #{inspect(decision.status)}"
+      )
     end)
+  end
+
+  def credit_decision(request) do
+    Logger.info("Final decision on credit request: #{inspect(request)}")
+
+    {credit_requested, _} = Integer.parse(request.credit_requested)
+    Logger.info("Credit requested: #{inspect(credit_requested)}")
+
+    final_decision =
+      case {request.status, credit_requested} do
+        {"EXPERIAN_CHECK_DONE", _}
+        when credit_requested < 500_000 ->
+          "APPROVED"
+
+        {"EXPERIAN_CHECK_DONE", _}
+        when credit_requested > 500_000 and credit_requested < 1_000_000 ->
+          "EXCEPTION_REVIEW"
+
+        _ ->
+          "REJECTED"
+      end
+
+    request_processed = %{
+      request
+      | status: final_decision,
+        activity_log: Enum.concat(request.activity_log, ["CREDIT_DECISION"])
+    }
+
+    request_processed
   end
 end
--- lib/event_producer_consumer.ex
+++ lib/event_producer_consumer.ex
@@ -5,6 +5,9 @@ defmodule EventProducerConsumer do
   use GenStage
   require Logger
 
+  require Application
+  @experian_endpoint Application.compile_env(:ref_elixir_genstage_pipeline, :experian_endpoint)
+
   def start_link(id) do
     initial_state = []
     GenStage.start_link(__MODULE__, initial_state, name: via(id))
@@ -26,6 +29,41 @@ defmodule EventProducerConsumer do
 
   def handle_events(events, from, state) do
     Logger.info("Event ProducerConsumer received #{inspect(events)} from #{inspect(from)}")
-    {:noreply, events, state}
+
+    new_events = Enum.map(events, &experian_check/1)
+    Logger.info("Event ProducerConsumer transformed #{inspect(events)} to #{inspect(new_events)}")
+
+    {:noreply, new_events, state}
+  end
+
+  def experian_check(request) do
+    Logger.info("Request for Experian Check: #{inspect(request)}")
+
+    api_response = HTTPoison.get!(@experian_endpoint)
+
+    response =
+      case api_response do
+        %HTTPoison.Response{status_code: 200} ->
+          %{
+            request_id: request.request_id,
+            request_type: "Experian Check",
+            status_code: 200
+          }
+
+        _ ->
+          %{
+            request_id: request.request_id,
+            request_type: "Experian Check",
+            status_code: :error
+          }
+      end
+
+    request_processed = %{
+      request
+      | status: "EXPERIAN_CHECK_DONE",
+        activity_log: Enum.concat(request.activity_log, ["EXPERIAN_CHECK"])
+    }
+
+    request_processed
   end
 end
